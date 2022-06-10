8da677ea045c791ca28f01a81302c4c82304252b
Rohitshelar13 <rohitshelar132001@gmail.com>
2022-06-10 14:50:48 +0530

    final commit

--- CareerMounters/settings.py
+++ CareerMounters/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-id(q-i_d(uk#xnwbos@u8$*^8%fsr^awojh=7ne^!o1b%m+cx6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','careermounters.herokuapp.com']
 
 
 # Application definition
